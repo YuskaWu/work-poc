9c964d47529e8f557e4073f1d562cacbed505f52
Yuska Wu <yuska@msn.com>
2020-04-29 19:07:59 +0800

    add poc for civilian superstar project

---
+++ civilian-superstart/precache-manifest.19e8b84c637d5c7f981c7b8ae9470f9d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87a107dfd2bb4876706e",
+    "url": "/work-poc/civilian-superstart/css/app.e6bdc0d5.css"
+  },
+  {
+    "revision": "9275035f28b985fd4873859e7980935c",
+    "url": "/work-poc/civilian-superstart/data.json"
+  },
+  {
+    "revision": "8ce2a861c771ef16b36ae9ed2a7ecc2b",
+    "url": "/work-poc/civilian-superstart/index.html"
+  },
+  {
+    "revision": "87a107dfd2bb4876706e",
+    "url": "/work-poc/civilian-superstart/js/app.f7d15a3b.js"
+  },
+  {
+    "revision": "10b15bbdcabe4f649bc2",
+    "url": "/work-poc/civilian-superstart/js/chunk-vendors.ddfadcc8.js"
+  },
+  {
+    "revision": "db9bab40d09d1416d1d88e630aa54ee1",
+    "url": "/work-poc/civilian-superstart/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/work-poc/civilian-superstart/robots.txt"
+  },
+  {
+    "revision": "1093f642a72b47e50f9495fae5810e05",
+    "url": "/work-poc/civilian-superstart/videojs-static.html"
+  }
+]);
